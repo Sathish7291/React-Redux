6622d96d50f309208736ecd5cd6d1c7bf440d2db
sathish7291 <sathish010395@gmail.com>
2024-08-15 21:09:58 +0530

    react-redux

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCard from './components/ProductCard'
+import Cart from './components/Cart'
+
+
+const App = () => {
+  return (
+    <div>
+      <ProductCard/>
+    </div>
+  )
+}
+
+export default App
